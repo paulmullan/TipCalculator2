ff442ee0b2a5395d8d61f43e761b053e1131b4a1
Paul Mullan <paul.mullan42@gmail.com>
2016-05-13 15:57:05 +1000

    Completed TipCalculator project - stage 1.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalTextField: UITextField!
+    @IBOutlet weak var taxPctSlider: UISlider!
+    @IBOutlet weak var taxPctLabel: UILabel!
+    @IBOutlet weak var resultsTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
-
+    
+    @IBAction func calculatorTapped(sender: UIButton) {
+        tipCalc.total = Double((totalTextField.text! as NSString).doubleValue)
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        //let sortedPossibleTips = sorted(possibleTips) { $0.0 < $1.0 }
+        for (tipPct, tipVal) in possibleTips {
+            results += "\(tipPct)%: \(String(format: "%0.2f", tipVal))\n"
+        }
+        resultsTextView.text = results
+    }
+    
+    @IBAction func taxPercentageChanged(sender: UISlider) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
+    }
+    
+    @IBAction func viewTapped(sender: UITapGestureRecognizer) {
+        totalTextField.resignFirstResponder()
+    }
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI()
+    {
+        //print("Total: \(tipCalc.total)")
+        totalTextField.text = String(tipCalc.total)
+        //print("String format = \(String(format: "0.2f", tipCalc.total))")
+        taxPctSlider.value = Float(tipCalc.taxPct * 100.0)
+        taxPctLabel.text = "Tax Percent (\(Int(taxPctSlider.value)))%:"
+        resultsTextView.text = ""
+    }
+    
 }
 
